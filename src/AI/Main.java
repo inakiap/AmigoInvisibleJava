d0a1b66c4a6005415bf63dc8bc6eb1751ac8614b
Iñaki Alfaro <inakiap@gmail.com>
2021-11-15 10:23:49 +0100

    Corrección de participantes en el sorteo sin personas que no les puedan tocar.

--- src/AI/Main.java
+++ src/AI/Main.java
@@ -7,70 +7,64 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//String to = "inakiap@gmail.com";
-		//String subject = "Test Gmail";
-		//String body = "Texto del mensaje";
-		//SendEmailSSL.sendEmail(to, subject, body);
+		// String to = "inakiap@gmail.com";
+		// String subject = "Test Gmail";
+		// String body = "Texto del mensaje";
+		// SendEmailSSL.sendEmail(to, subject, body);
 		try {
-			sorteo(true);
+			sorteo(false);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-						
+
 	}
 
 	public static void sorteo(Boolean envio) throws IOException {
 		// Leer los datos de los participantes
 		System.out.println("*** CONFIGURACION");
 		System.out.println("\n");
-		
+
 		Participante participantes[] = Configuracion.ObtenerParticipantes();
 		System.out.println("\n");
 		System.out.println("*** EMPIEZA EL SORTEO");
 		System.out.println("\n");
-		
+
 		ArrayList<Participante[]> resultadoSorteo = Permutaciones.Sorteo(participantes);
 		System.out.println("\n");
 		System.out.println("*** ENVIO DE LOS CORREOS");
 		System.out.println("\n");
 		System.out.println("Pulsa ENTER para continuar");
 		System.in.read();
-		
+
 		int contador = 1;
 		for (Participante[] participante : resultadoSorteo) {
-				
+
 			String toEmail = participante[0].email;
 			String subject = "Resultado del sorteo del amigo invisible 2019";
-			String body = "¡Hola! " + participante[0].nombre +"\n\n"
-					+ "Tu amigo invisible es.........." + participante[1].nombre + "\n\n\n\n"
-					+ "Y RECUERDA, NO CONTESTES A ESTE CORREO \n"
-					+ "Mejor envia un correo nuevo si me quieres confirmar que te llegó, en principio yo sé que si se envía o no."
-					+ "\n\n"
-					+ "Ahora solo faltan las listas, eso sí que sí Mándalo al grupo:\n"
-					+ "amigoinvisiblePL@googlegroups.com"
-					+ "\n\n"
-					+ "Un beso,\n"
-					+ "Iñaki";
-			
+			String body = "Â¡Hola! " + participante[0].nombre + "\n\n" + "Tu amigo invisible es.........."
+					+ participante[1].nombre + "\n\n\n\n" + "Y RECUERDA, NO CONTESTES A ESTE CORREO \n"
+					+ "Mejor envia un correo nuevo si me quieres confirmar que te llegÃ³, en principio yo sÃ© si se envÃ­a o no."
+					+ "\n\n" + "Ahora solo faltan las listas, eso sÃ­ que sÃ­ mÃ¡ndalo al grupo:\n"
+					+ "amigoinvisiblePL@googlegroups.com" + "\n\n" + "Un beso,\n" + "IÃ±aki";
+
 			System.out.print(contador + " ");
 			System.out.print(toEmail + "	\n");
 			System.out.print(subject + "	\n");
 			System.out.print(body);
 			System.out.println("\n");
-			
+
 			if (envio) {
-				SendEmailSSL.sendEmail(toEmail, subject, body);  
+				SendEmailSSL.sendEmail(toEmail, subject, body);
 			}
-			
-			contador ++;
+
+			contador++;
 		}
-		System.out.println("\n");	
-		System.out.println("*** ACABÓ EL SORTEO");
-		
+		System.out.println("\n");
+		System.out.println("*** ACABÃ“ EL SORTEO");
+
 		System.in.read();
 
 	}
-
 
 }
--- src/AI/SendEmailSSL.java
+++ src/AI/SendEmailSSL.java
@@ -6,88 +6,79 @@ import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class SendEmailSSL {
-	
-	 public static void envioTest() {
 
-	        final String username = "addmminnn@gmail.com";
-	        final String password = "Bambole,77";
+	public static void envioTest() {
 
-	        Properties prop = new Properties();
-			prop.put("mail.smtp.host", "smtp.gmail.com");
-	        prop.put("mail.smtp.port", "465");
-	        prop.put("mail.smtp.auth", "true");
-	        prop.put("mail.smtp.socketFactory.port", "465");
-	        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-	        
-	        Session session = Session.getInstance(prop,
-	                new javax.mail.Authenticator() {
-	                    protected PasswordAuthentication getPasswordAuthentication() {
-	                        return new PasswordAuthentication(username, password);
-	                    }
-	                });
+		final String username = "addmminnn@gmail.com";
+		final String password = "Bambole,77";
 
-	        try {
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "465");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.socketFactory.port", "465");
+		prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
-	            Message message = new MimeMessage(session);
-	            message.setFrom(new InternetAddress("addmminnn@gmail.com"));
-	            message.setRecipients(
-	                    Message.RecipientType.TO,
-	                    InternetAddress.parse("inakiap@posteo.net")
-	            );
-	            message.setSubject("Testing Gmail SSL");
-	            message.setText("Dear Mail Crawler,"
-	                    + "\n\n Please do not spam my email!");
+		Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
 
-	            Transport.send(message);
+		try {
 
-	            System.out.println("Done\n");
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("addmminnn@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("inakiap@posteo.net"));
+			message.setSubject("Testing Gmail SSL");
+			message.setText("Dear Mail Crawler," + "\n\n Please do not spam my email!");
 
-	        } catch (MessagingException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	 
-	 public static Boolean sendEmail(String to, String subject, String body) {
+			Transport.send(message);
 
-	        final String username = "addmminnn@gmail.com";
-	        final String password = "Bambole,77";
-	        
-	        Boolean response = false;
+			System.out.println("Done\n");
 
-	        Properties prop = new Properties();
-			prop.put("mail.smtp.host", "smtp.gmail.com");
-	        prop.put("mail.smtp.port", "465");
-	        prop.put("mail.smtp.auth", "true");
-	        prop.put("mail.smtp.socketFactory.port", "465");
-	        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-	        
-	        Session session = Session.getInstance(prop,
-	                new javax.mail.Authenticator() {
-	                    protected PasswordAuthentication getPasswordAuthentication() {
-	                        return new PasswordAuthentication(username, password);
-	                    }
-	                });
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
 
-	        try {
+	public static Boolean sendEmail(String to, String subject, String body) {
 
-	            Message message = new MimeMessage(session);
-	            message.setFrom(new InternetAddress("addmminnn@gmail.com"));
-	            message.setRecipients(
-	                    Message.RecipientType.TO,
-	                    InternetAddress.parse(to)
-	            );
-	            message.setSubject(subject);
-	            message.setText(body);
+		final String username = "addmminnn@gmail.com";
+		final String password = "Bambole,77";
 
-	            Transport.send(message);
-	            response = true;
+		Boolean response = false;
 
-	            System.out.println("Enviado a "+to+"\n");
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "465");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.socketFactory.port", "465");
+		prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
-	        } catch (MessagingException e) {
-	            e.printStackTrace();
-	        }
-	        return response;
-	    }
+		Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("addmminnn@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			message.setSubject(subject);
+			message.setText(body);
+
+			Transport.send(message);
+			response = true;
+
+			System.out.println("Enviado a " + to + "\n");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
 
 }
